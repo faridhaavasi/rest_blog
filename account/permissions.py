45827af359b25b8883930999925b0f7d8c267ce1
faridhaavasi <faridhavasi777@gmail.com>
2023-07-08 18:50:21 +0330

    permatissns

---
+++ account/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+from .models import Blacked
+class BlocklistPermission(permissions.BasePermission):
+
+
+    def has_permission(self, request, view):
+        username = request.user.username
+        blocked = Blacked.objects.filter(username=username).exists()
+        return not blocked
--- rest_blog/settings.py
+++ rest_blog/settings.py
@@ -133,6 +133,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
 
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'account.permissions.BlocklistPermission'
     ]
 }
 
